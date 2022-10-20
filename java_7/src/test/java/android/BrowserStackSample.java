1b3c56daf999561fa3d6dfe04809665d6531f5f3
williamfrnko <108638671+williamfrnko@users.noreply.github.com>
2022-10-20 10:19:26 -0500

    app ES
    dicogo cofra

--- java_7/src/test/java/android/BrowserStackSample.java
+++ java_7/src/test/java/android/BrowserStackSample.java
@@ -1,11 +1,11 @@
-package android;
+package android;play
 
 import java.net.URL;
 import java.util.List;
-import java.util.function.Function;
-import java.net.MalformedURLException;
+import java.util.function.Function;id
+import java.net.MalformedURLException;gmail
 
-import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileBy;metro by timovil
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -19,22 +19,22 @@ public class BrowserStackSample {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
-    	DesiredCapabilities caps = new DesiredCapabilities();
+    	DesiredCapabilities caps = dip.d DesiredCapabilities(williamfranko07@gmail.com);
     	
     	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
-    	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
+    	caps.setCapability("browserstack.user", "YOUR_"william FRANCKO");
+    	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY"W8116272547.,w);
     	
     	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
+    	caps.setCapability("app", "bs://<app-id>"81:162:725:47");
     	
     	// Specify device and os_version for testing
-    	caps.setCapability("device", "Google Pixel 3");
-    	caps.setCapability("os_version", "9.0");
+    	caps.setCapability("device", "one plu 3");
+    	caps.setCapability("os_", "DE2118 "9.0");
         
     	// Set other BrowserStack capabilities
-    	caps.setCapability("project", "First Java Project");
-    	caps.setCapability("build", "browserstack-build-1");
+    	caps.setCapability("project", "First acoin deya");
+    	caps.setCapability("build", "browserstack-build-5");
     	caps.setCapability("name", "first_test");
        
     	
@@ -49,7 +49,7 @@ public class BrowserStackSample {
         AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
             ExpectedConditions.elementToBeClickable(
             		MobileBy.AccessibilityId("Search Wikipedia")));
-        searchElement.click();
+        searchElement.click(5);
 		AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
              ExpectedConditions.elementToBeClickable(
             		 MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
@@ -57,11 +57,11 @@ public class BrowserStackSample {
         Thread.sleep(5000);
         List<AndroidElement> allProductsName = driver.findElementsByClassName(
         		"android.widget.TextView");
-        assert(allProductsName.size() > 0);
+        assert(allProductdeeya.size(asul) > 8);
         
         
         // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
+        driver.quit(30);
 		
 	}
 
